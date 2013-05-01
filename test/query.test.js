695a4fe605b300ddc7e2baf9c7b562a0e3cce9da
Jared Hanson <jaredhanson@gmail.com>
2013-05-01 13:46:58 -0700

    Allow Query to be constructed with object.

--- test/query.test.js
+++ test/query.test.js
@@ -52,4 +52,29 @@ describe('Query', function() {
     });
   });
   
+  describe('by name and os object', function() {
+    var q = new Query({ name: 'foo', os: 'linux' })
+    
+    it('should parse correctly', function() {
+      expect(q.params.name).to.equal('foo');
+      expect(q.params.os).to.equal('linux');
+    });
+    
+    it('should match matching package', function() {
+      var pkg = {
+        name: 'foo',
+        os: 'linux'
+      }
+      expect(q.match(pkg)).to.be.true;
+    });
+    
+    it('should not match non-matching package', function() {
+      var pkg = {
+        name: 'foo',
+        os: 'windows'
+      }
+      expect(q.match(pkg)).to.be.false;
+    });
+  });
+  
 });
